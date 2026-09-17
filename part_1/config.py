cc588ff88975f91fdf2fed808acd7b2e2aa6ad70
Borisimo <boris.chaman@gmail.com>
2026-09-17 17:14:45 +0200

    Adjust PID gains for improved control parameters
    Previuos values that did okay but still failed 2 tests: 
    wn_N   = 0.4
    wn_E   = 0.4
    wn_psi = 0.3
    
    zeta_N   = 1.0
    zeta_E   = 1.0
    zeta_psi = 1.0

--- part_1/config.py
+++ part_1/config.py
@@ -79,13 +79,13 @@ M = M_RB + M_A
 
 @dataclass
 class PIDGains:
-    wn_N   = 0.4
-    wn_E   = 0.4
-    wn_psi = 0.3
+    wn_N   = 0.22
+    wn_E   = 0.22
+    wn_psi = 0.2
 
-    zeta_N   = 1.0
-    zeta_E   = 1.0
-    zeta_psi =  1.0
+    zeta_N   = 1.1
+    zeta_E   = 1.1
+    zeta_psi =  1.1
 
     Kp: np.ndarray = field(init=False)
     Ki: np.ndarray = field(init=False)
