1f9d67b3c5a449e2e40de57a46f4722a1bc5a48a
vegava352 <vegava@stud.ntnu.no>
2026-09-24 13:03:18 +0200

    Adjust Ki calculation for improved scaling factors

--- part_1/config.py
+++ part_1/config.py
@@ -99,9 +99,9 @@ class PIDGains:
         ])
             
         self.Ki = np.array([
-            (self.wn_N/50) * self.Kp[0], 
-            (self.wn_E/50) * self.Kp[1], 
-            (self.wn_psi/50) * self.Kp[2]
+            (self.wn_N/10) * self.Kp[0], 
+            (self.wn_E/10) * self.Kp[1], 
+            (self.wn_psi/10) * self.Kp[2]
         ])
             
         self.Kd = np.array([
